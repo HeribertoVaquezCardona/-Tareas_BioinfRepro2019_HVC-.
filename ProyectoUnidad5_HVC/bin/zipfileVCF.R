19a971497032ce3f30e0ecdfb861bec40cf97406
HeribertoVaquezCardona <44846726+HeribertoVaquezCardona@users.noreply.github.com>
2019-03-26 11:26:02 -0600

    Add files via upload

---
+++ ProyectoUnidad5_HVC/bin/zipfileVCF.R
@@ -0,0 +1,6 @@
+# Zip "/data/filtered/Zea_mays_7LR.vcf
+# This script will make "Zea_mays_7LR.vcf.gz", this file is necesary to convert VCF to genlight
+
+
+gzip -9 /data/filtered/Zea_mays_7LR.vcf 
+
