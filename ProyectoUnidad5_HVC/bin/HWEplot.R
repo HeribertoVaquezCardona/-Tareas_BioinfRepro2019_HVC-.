13f1bde245558caa216c69357d7d1f6054b9509c
HeribertoVaquezCardona <44846726+HeribertoVaquezCardona@users.noreply.github.com>
2019-04-02 19:15:51 -0600

    Update HWEplot.R

--- ProyectoUnidad5_HVC/bin/HWEplot.R
+++ ProyectoUnidad5_HVC/bin/HWEplot.R
@@ -1,4 +1,15 @@
-# This scripts creates the HWE plot.
+# This script creates the HWE plot.
 
-install.packages("glPlot", dependencies = TRUE)
-library(glPlot)
+# This plot uses chi² in the "y" and the 2 popolutions in "x", but the avarages aren´t the best because I used just some
+# samples, not all :s .
+
+
+
+chi2<-c(3.64E+06, )
+Popolutions<-c(Vandeno,Chalqueno)
+barplot(x=Popolutions, y=chi²)
+
+
+
+HWEPlot <- data.frame(popolution=c("Vandeno", "Chalqueno"), chi2=c(3.6406, 4.3804))
+barplot(HWEPlot$chi2, names.arg= HWEPlot$popolution)
